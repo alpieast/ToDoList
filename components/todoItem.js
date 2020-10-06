d7db08cdf8db42ac3a24362955c7d51497c05be3
Alperen Dogu <dogualperen@gmail.com>
2020-10-06 11:11:39 +0300

    visual improvements

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity,View } from 'react-native';
 
 export default function TodoItem({item, pressHandler}){
 
     return(
-        <TouchableOpacity onPress={ () => pressHandler(item.key)}>
-        <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
+
+        <View style={styles.item}>
+
+            <Text style={styles.todoText}>{item.text}</Text>
+
+            <TouchableOpacity style={styles.delete} onPress={ () => pressHandler(item.key)}>
+                <Text style={styles.deleteText}>Delete</Text>
+            </TouchableOpacity>
+
+        </View>
+        
 
     )
 };
@@ -15,9 +23,17 @@ const styles = StyleSheet.create({
     item: {
         padding:16,
         marginTop:16,
-        borderColor: '#bbb',
+        borderColor: 'coral',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection: 'row'
+    },
+    deleteText:{
+        flex:1,
+        color: 'red'
+    },
+    todoText:{
+        flex: 5
     }
 })
