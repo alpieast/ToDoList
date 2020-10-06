4bb772501e221faa9b07417dd243e9161691c9e5
Alperen Dogu <dogualperen@gmail.com>
2020-10-06 10:01:07 +0300

    header added

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import Header from './components/header'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'react ogren', key: '1'},
+    {text: 'pratik yap', key: '2'},
+    {text: 'uygulamaya gec', key: '3'}
+  ]);
+
   return (
     <View style={styles.container}>
-      
+      {/*header */}
+      <Header />
+      <View style={styles.content}>
+        {/*To Form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+            <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+    
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
