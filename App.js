ec8952499833aa7db7066706ed68ade088777490
Alperen Dogu <dogualperen@gmail.com>
2020-10-06 10:19:30 +0300

    erase function added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
 import { StyleSheet, Text, View,FlatList } from 'react-native';
 import Header from './components/header'
+import TodoItem from './components/todoItem'
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -10,9 +11,14 @@ export default function App() {
     {text: 'uygulamaya gec', key: '3'}
   ]);
 
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      {/*header */}
       <Header />
       <View style={styles.content}>
         {/*To Form */}
@@ -20,7 +26,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-            <Text>{item.text}</Text>
+            <TodoItem item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
